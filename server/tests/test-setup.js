2bb00f866aedb4f6eb2387dd593254861ff71c99
Tran Vu Quang Anh <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2025-01-14 03:24:17 +0700

    Update test-setup.js

--- server/tests/test-setup.js
+++ server/tests/test-setup.js
@@ -1,8 +1,33 @@
+// const mongoose = require('mongoose');
+// const { MongoMemoryServer } = require('mongodb-memory-server');
+
+
+// let mongoServer;
+
+// beforeAll(async () => {
+//   mongoServer = await MongoMemoryServer.create({
+//     instance: {
+//       port: 27017, // Use a specific port
+//     },
+//   });
+//   // const uri = mongoServer.getUri();
+
+//   mongoose.set('strictQuery', false); 
+//    await mongoose.connect(process.env.MONGO_URI, {
+//          useNewUrlParser: true,
+//          useUnifiedTopology: true
+//   });
+// });
+
+// afterAll(async () => {
+//   await mongoose.disconnect();
+//   if (mongoServer) await mongoServer.stop();
+// });
+
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
-
-
-let mongoServer;
+const app = require('../index'); // Ensure you're importing your app
+let mongoServer, server;
 
 beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create({
@@ -10,16 +35,19 @@ beforeAll(async () => {
       port: 27017, // Use a specific port
     },
   });
-  // const uri = mongoServer.getUri();
 
-  mongoose.set('strictQuery', false); 
-   await mongoose.connect(process.env.MONGO_URI, {
-         useNewUrlParser: true,
-         useUnifiedTopology: true
+  mongoose.set('strictQuery', false);
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
+
+  // Start the server
+  server = app.listen(process.env.PORT || 3000, '0.0.0.0');
 });
 
 afterAll(async () => {
   await mongoose.disconnect();
   if (mongoServer) await mongoServer.stop();
+  if (server) server.close(); // Ensure server is closed
 });
