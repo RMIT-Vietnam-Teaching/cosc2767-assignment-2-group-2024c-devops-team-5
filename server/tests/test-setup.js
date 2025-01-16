19a8fb25e8cf916789822b0a785f8c03911589e4
Tran Vu Quang Anh <153184535+tranvuquanganhRMIT87@users.noreply.github.com>
2025-01-16 18:54:26 +0700

    fix and testCI 18:54

--- server/tests/test-setup.js
+++ server/tests/test-setup.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const { MongoMemoryServer } = require('');
-mongodb-memory-server-core
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
 
 let mongoServer;
 
